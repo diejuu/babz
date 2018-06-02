123617f5f0b43b81e0612ca9f55e9faae642f18e
diejuu <hannahwanjiku510@gmail.com>
2018-06-02 10:50:04 -0700

    first commit

---
+++ nana.py
@@ -0,0 +1,20 @@
+my_list=[1,4,5,6]
+print(my-list)
+
+my_list 2["Hannah", "Wanjiku",]
+print(my-list 2)
+
+
+my_list 3["mama",23 "papa",45]
+
+print(my-list 3)
+
+my_tuple=(1,2,3,4)
+print(my_tuple)
+
+my_set=set(1,4,"Hannah","shiko",6)
+print(my_set)
+set([1,4,"Hannah","shiko",6])
+
+my_dict={"Name":"Hannah","Age":21,"Nationality":"Kenyan","Address":"Nairobi"}
+print(my_dict)
